c88766e2f616630209ae2355fdc4c7173eb50b5d
RuanZzzz <734162396@qq.com>
2022-04-04 18:02:03 +0800

    IOC容器—Bean管理注解方式（创建对象_测试）

--- src/com/richard/spring5_2/service/UserService.java
+++ src/com/richard/spring5_2/service/UserService.java
@@ -1,12 +1,14 @@
 package com.richard.spring5_2.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 // 注解中的value属性值可以省略不写
 // 默认值是类名称，首字母小写
-@Component(value = "userService") // 等于 <bean id="" class="" />
+//@Component(value = "userService") // 等于 <bean id="" class="" />
+@Service
 public class UserService {
     public void add() {
-        System.out.println("service add...");
+        System.out.println("service add test...");
     }
 }
